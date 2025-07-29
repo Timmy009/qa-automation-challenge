3bc652ed08f2e670ae095639d506f046a4b7a571
Timmy009 <admin@mycoursemate.com>
2025-07-29 02:11:39 +0100

    imitial commit

--- app/frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/timileyinbamgbose/Downloads/qa-automation-challenge (37)/app/frontend/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
